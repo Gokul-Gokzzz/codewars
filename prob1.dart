086fcd3e23eaf317e118108f888af9cfa3b02901
Gokul-Gokzzz <gokulunniu88@gmail.com>
2023-10-17 19:39:29 +0530

    first commit

---
+++ prob1.dart
@@ -0,0 +1,44 @@
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// void main(){
+//   List<int> arr=[1,-2,-4,5,7]; 
+
+//   int sum = positive(arr);
+//   print('Sum of posiive numbers : $sum');
+
+// }
+
+
+//   int positive(List<int>arr){
+// int sum =0;
+//   for(var x in arr){
+//     if(x>0){
+//     sum+=x;
+  
+//      }
+//   }
+//     return sum;
+//   }
+
+
+
+// Find the smallest integer in the array
+
+// void main(){
+// List<int> arr = [12,425,53,-12,-134,97];
+// int result = findSmallestInt(arr);
+// print('Smallest number is: $result');
+// }
+
+// int findSmallestInt(List<int> arr) {
+//   int small = arr[0];
+//   for (int x in arr) {
+//     if (x < small) {
+//       small = x;
+//     }
+//   }
+//   return small;
+// }
+
+
